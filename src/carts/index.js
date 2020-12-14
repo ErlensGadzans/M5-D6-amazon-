9a02c1254954ddaea9ab14ab27c8c4cb829980d8
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-12-14 16:33:14 +0200

    router get with id added

--- src/carts/index.js
+++ src/carts/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const uniqid = require("uniqid");
 const { readDB } = require("../lib/utilities"); //i dont really understand what does it is for
 
 const router = express.Router();
@@ -11,6 +13,27 @@ router.get("/", async (req, res, next) => {
     const cartDataBase = await readDB(cartsFilePath);
     if (cartDataBase.length > 0) {
       res.status(201).send(cartDataBase);
+    } else {
+      const error = {};
+      error.httpStatusCode = 404;
+      error.message = "The cart is empty!";
+      next(error);
+    }
+  } catch (error) {
+    error.httpStatusCode = 404;
+    next(error);
+  }
+});
+
+router.get("/:cartId", async (req, res, next) => {
+  try {
+    const cartDataBase = await readDB(cartsFilePath);
+    const selectedCart = cartDataBase.findIndex(
+      (cart) => cart._id === req.params.cartId
+    );
+
+    if (selectedCart !== -1) {
+      res.status(201).send(cartDataBase[selectedCart]);
     } else {
       const error = {};
       error.httpStatusCode = 404;
